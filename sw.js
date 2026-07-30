16e443d7cecc566cad42f213d18665afaf5c82e7
borzeeb-coder <borzeeb-coder@users.noreply.github.com>
2026-07-30 09:31:48 +0200

    restore v52-stable backup, remove SW reload bug

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ const STATIC = [
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE).then((c) => c.addAll(STATIC)).then(() => self.skipWaiting())
+    caches.open(CACHE).then((c) => c.addAll(STATIC))
   );
 });
 
@@ -16,7 +16,7 @@ self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    )
   );
 });
 
